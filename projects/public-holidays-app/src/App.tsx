3c99e670fe5542135aab9c5a86f50cb616eafaa7
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-24 20:09:29 +0200

    Refactor App component to use CountrySelect for country selection
    - Extracted the country selection dropdown into a new CountrySelect component for better modularity.
    - Integrated the useCountries hook within the CountrySelect component to fetch and display country options.
    - Updated the App component to utilize the new CountrySelect, improving code organization and readability.

--- projects/public-holidays-app/src/App.tsx
+++ projects/public-holidays-app/src/App.tsx
@@ -6,34 +6,52 @@ import { useCountries } from "./data/use-countries"
 import type { IsoCode } from "./types"
 
 function App() {
-  const { data: countries } = useCountries()
-
   const [selectedCountry, setSelectedCountry] = useState<IsoCode>(
     DEFAULT_COUNTRY_ISO_CODE
   )
 
   return (
     <main>
-      <select
-        disabled={!countries}
-        value={selectedCountry}
-        onChange={(e) => setSelectedCountry(e.target.value as IsoCode)}
-      >
-        {!countries ? (
-          <option value={selectedCountry}>Loading Countries...</option>
-        ) : (
-          countries?.map((country) => (
-            <option
-              id={`select-option-${country.isoCode}`}
-              key={country.isoCode}
-              value={country.isoCode}
-            >
-              {country.name}
-            </option>
-          ))
-        )}
-      </select>
+      <CountrySelect
+        selectedCountry={selectedCountry}
+        setSelectedCountry={setSelectedCountry}
+      />
     </main>
+  )
+}
+
+interface CountrySelectProps {
+  selectedCountry: IsoCode
+  setSelectedCountry: (country: IsoCode) => void
+}
+
+function CountrySelect({
+  selectedCountry,
+  setSelectedCountry,
+}: CountrySelectProps) {
+  const { data: countries } = useCountries()
+
+  return (
+    <select
+      id="country-select"
+      disabled={!countries}
+      value={selectedCountry}
+      onChange={(e) => setSelectedCountry(e.target.value as IsoCode)}
+    >
+      {!countries ? (
+        <option value={selectedCountry}>Loading Countries...</option>
+      ) : (
+        countries?.map((country) => (
+          <option
+            id={`country-select-option-${country.isoCode}`}
+            key={country.isoCode}
+            value={country.isoCode}
+          >
+            {country.name}
+          </option>
+        ))
+      )}
+    </select>
   )
 }
 
