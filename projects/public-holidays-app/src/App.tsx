e36c2db3a01aa9fd8980c1761176384201bae0c4
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-24 17:04:09 +0200

    Refactor App component to integrate country selection
    - Removed previous static content and replaced it with a dynamic country selection dropdown.
    - Utilized the `useCountries` hook to fetch and display country options.
    - Set the default selected country using the `DEFAULT_COUNTRY_ISO_CODE` constant.

--- projects/public-holidays-app/src/App.tsx
+++ projects/public-holidays-app/src/App.tsx
@@ -1,35 +1,39 @@
 import { useState } from "react"
 
 import "./App.css"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
+import { DEFAULT_COUNTRY_ISO_CODE } from "./constants"
+import { useCountries } from "./data/use-countries"
+import type { IsoCode } from "./types"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data: countries } = useCountries()
+
+  const [selectedCountry, setSelectedCountry] = useState<IsoCode>(
+    DEFAULT_COUNTRY_ISO_CODE
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <select
+        disabled={!countries}
+        value={selectedCountry}
+        onChange={(e) => setSelectedCountry(e.target.value as IsoCode)}
+      >
+        {!countries ? (
+          <option value={selectedCountry}>Loading Countries...</option>
+        ) : (
+          countries?.map((country) => (
+            <option
+              id={`select-option-${country.isoCode}`}
+              key={country.isoCode}
+              value={country.isoCode}
+            >
+              {country.name}
+            </option>
+          ))
+        )}
+      </select>
+    </main>
   )
 }
 
