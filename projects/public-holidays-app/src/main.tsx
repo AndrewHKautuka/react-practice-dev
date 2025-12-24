081eff827d4fec683028d4fd7effb3bdee08749d
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-24 15:03:38 +0200

    Add React Query integration for data fetching
    - Introduced a new `query-client.ts` file to configure the QueryClient from @tanstack/react-query.
    - Wrapped the App component in `QueryClientProvider` in `main.tsx` to enable React Query functionality.

--- projects/public-holidays-app/src/main.tsx
+++ projects/public-holidays-app/src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react"
 
+import { QueryClientProvider } from "@tanstack/react-query"
 import { createRoot } from "react-dom/client"
 
+import { queryClient } from "../../../lib/query-client.ts"
 import App from "./App.tsx"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 )
