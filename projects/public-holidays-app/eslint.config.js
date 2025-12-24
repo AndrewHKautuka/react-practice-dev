e77096950dd55dbb20777065f7f95fc01a958236
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-24 14:34:03 +0200

    Enhance ESLint configuration with Prettier integration
    - Added Prettier configuration and plugin to ESLint setup for improved code formatting.
    - Updated ESLint rules to enforce Prettier formatting as errors.
    - Ensured compatibility with both JavaScript and TypeScript files.

--- projects/public-holidays-app/eslint.config.js
+++ projects/public-holidays-app/eslint.config.js
@@ -1,5 +1,7 @@
 import js from "@eslint/js"
 import pluginQuery from "@tanstack/eslint-plugin-query"
+import prettierConfig from "eslint-config-prettier/flat"
+import prettier from "eslint-plugin-prettier"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
 import { defineConfig, globalIgnores } from "eslint/config"
@@ -8,6 +10,7 @@ import tseslint from "typescript-eslint"
 
 export default defineConfig([
   globalIgnores(["dist"]),
+  prettierConfig,
   ...pluginQuery.configs["flat/recommended"],
   {
     files: ["**/*.{ts,tsx}"],
@@ -20,6 +23,15 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
     },
   },
 ])
