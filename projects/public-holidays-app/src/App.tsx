33ddbede67337b1ac9f228afbcf194b7386a2dea
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-26 14:11:49 +0200

    Integrate public holidays display in App component
    - Added `PublicHolidayList` component to fetch and display public holidays based on the selected country and current year.
    - Utilized the `usePublicHolidays` hook to retrieve holiday data and formatted the display using `date-fns` for better date representation.
    - Updated the `App` component to include the new `PublicHolidayList`, enhancing the app's functionality and user experience.

--- projects/public-holidays-app/src/App.tsx
+++ projects/public-holidays-app/src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react"
 
+import { format, getYear } from "date-fns"
+
 import "./App.css"
 import { DEFAULT_COUNTRY_ISO_CODE } from "./constants"
 import { useCountries } from "./data/use-countries"
+import { usePublicHolidays } from "./data/use-public-holidays"
 import type { IsoCode } from "./types"
 
 function App() {
@@ -10,12 +13,16 @@ function App() {
     DEFAULT_COUNTRY_ISO_CODE
   )
 
+  const year = getYear(Date())
+
   return (
     <main>
       <CountrySelect
         selectedCountry={selectedCountry}
         setSelectedCountry={setSelectedCountry}
       />
+
+      <PublicHolidayList selectedCountry={selectedCountry} year={year} />
     </main>
   )
 }
@@ -52,6 +59,27 @@ function CountrySelect({
         ))
       )}
     </select>
+  )
+}
+
+interface PublicHolidayProps {
+  selectedCountry: IsoCode
+  year: number
+}
+
+function PublicHolidayList({ selectedCountry, year }: PublicHolidayProps) {
+  const { data: publicHolidays } = usePublicHolidays(selectedCountry, year)
+
+  return !publicHolidays ? (
+    <p className="holiday-list-loading">Loading Public Holidays...</p>
+  ) : (
+    <ul className="holiday-list">
+      {publicHolidays.map((holiday) => (
+        <li key={holiday.id}>
+          {format(holiday.date, "d MMMM")} - {holiday.name}
+        </li>
+      ))}
+    </ul>
   )
 }
 
